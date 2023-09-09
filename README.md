06bb683e820fc98bea0c7c1e167520b18c27ec6d
unknown <fzembrzuski@gamil.com>
2023-09-09 23:10:48 +0200

    dopracownie trzonu projektu oraz nadanie pierwszej formy stronie głównej

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dziennik-treningowy
 An aplication which won't change anything in your life
+
+Firstly, I have to do a concept of main page.
+In my opinion I should use bright color like alicebule and white or dark like dark gray and light gray
+
--- start.py
+++ start.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Witaj świecie"
+def main():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=420,debug=True)
