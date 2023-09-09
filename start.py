371388b1ac024ff53df3a909e10dd74b830241ca
unknown <fzembrzuski@gamil.com>
2023-09-10 00:51:42 +0200

    dodanie nowego pliku z animacjami oraz poprawienie menu

--- start.py
+++ start.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=420,debug=True)
