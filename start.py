097adbe1fa84ad2d67180ece8acd3de7b9a77c80
Brzoza <fzembrzuski@gamil.com>
2023-10-02 22:16:31 +0200

    Dodanie tabeli do kalkulatora Stworzenie pliku database.py który tworzy tabelę i dodaje do niej dwie tabele, proportions oraz propCol Do dabeli propCol przypisane są dane początkowe W pliku start.py dodano zaczątek dodawanie własnych proporcji

--- start.py
+++ start.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+import sqlite3 as sql
+import database
 
 app = Flask(__name__)
+
+def add_prop():
+    conn = sql.connect("training'sDictionary.db")
+    
+    conn.execute("Insert Into 'proportions'(`date`, `height`, `knee`, `elbow`, `crimson`, `shoulder`, `chest`, `arm`, `calf`, `thigh`, `forearm`, `neck`) values ()")
 
 @app.route("/home")
 @app.route("/")
@@ -11,12 +18,28 @@ def main():
 def settings():
     return render_template("settings.html")
 
-@app.route("/calculator")
+@app.route("/calculator", methods = ["POST","GET"])
 def calc():
     return render_template("calculator.html")
 
+@app.route("/addProportions", methods = ["POST"])
+def addprop():
+    if request.method == "POST":
+        fnames = request.form
+        prop = []
+        
+        for n in fnames:
+            a = request.form[n]
+            prop.append(a)
+            
+        for i in prop:
+            print(i)
+        return redirect("/calculator")
+    else:
+        return redirect("/calculator")
+    
 @app.route("/dictionary")
-def dict():
+def diction():
     return render_template("dictionary.html")
 
 if __name__ == '__main__':
