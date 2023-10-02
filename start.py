8369ccc7d9e4cd957bf77a463015a2f9ef66d31a
Brzoza <fzembrzuski@gamil.com>
2023-10-02 21:40:20 +0200

    dodanie dwóch plików kalkulator, dziennik (.html) zmiana odnośników w menu z "#" na linki podstron oraz dodanie renderowanie tych podstron

--- start.py
+++ start.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/home")
 @app.route("/")
 def main():
     return render_template("index.html")
@@ -9,6 +10,14 @@ def main():
 @app.route("/settings")
 def settings():
     return render_template("settings.html")
+
+@app.route("/calculator")
+def calc():
+    return render_template("calculator.html")
+
+@app.route("/dictionary")
+def dict():
+    return render_template("dictionary.html")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=420,debug=True)
