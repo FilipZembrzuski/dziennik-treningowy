2cf844c41b4dfa3c1b30a2cb8483ac49fe743fc9
Brzoza <fzembrzuski@gamil.com>
2024-01-09 18:12:26 +0100

    można teraz zmieniać wcześniej podane dane

--- database.py
+++ database.py
@@ -8,8 +8,8 @@ if os.path.exists(db) != True:
     conn = sql.connect(db)
     #conn.execute("""CREATE TABLE `excercises` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT)""")
     conn.execute("""CREATE TABLE `proportions` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `date` DATE, `height` INT, `knee` INT, `elbow` INT, `crimson` INT, `shoulder` INT, `chest` INT, `arm` INT, `calf` INT, `thigh` INT, `forearm` INT, `neck` INT)""")
-    conn.execute("""CREATE TABLE `propCol` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT)""")
-    conn.execute("""Insert Into `propCol` (`name`) values ('date'), ('height'), ('knee'), ('elbow'), ('crimson'), ('shoulde'), ('ches'), ('ar'), ('cal'), ('thig'), ('forear'), ('nec')""")
+    conn.execute("""CREATE TABLE `propCol` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT, `type` TEXT)""")
+    conn.execute("""Insert Into `propCol` (`name`, `type`) values ('date', 'date'), ('height', 'number'), ('knee', 'number'), ('elbow', 'number'), ('crimson', 'number'), ('shoulder', 'number'), ('chest', 'number'), ('arm', 'number'), ('calf', 'number'), ('thigh', 'number'), ('forearm', 'number'), ('neck', 'number')""")
     conn.commit()
 else:
     conn = sql.connect(db)
--- start.py
+++ start.py
@@ -1,14 +1,99 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request
 import sqlite3 as sql
 import database
+import datetime as dt
 
 app = Flask(__name__)
 
-def add_prop():
+def read_prop_cols():
     conn = sql.connect("training'sDictionary.db")
+    propNames = conn.execute("Select name from propCol")
+    conn.commit()
     
-    conn.execute("Insert Into 'proportions'(`date`, `height`, `knee`, `elbow`, `crimson`, `shoulder`, `chest`, `arm`, `calf`, `thigh`, `forearm`, `neck`) values ()")
+    pn = []
+    
+    for i in propNames:
+        pn.append(i[0])
+        
+    return(pn)
+    
+def read_prop():
+    conn = sql.connect("training'sDictionary.db")
+    prop = conn.execute("Select * From 'proportions' Order by `date` desc")
+    conn.commit()
+    
+    pn = read_prop_cols()
+    proportions = []
 
+    for i in prop:
+        zmk = 0
+        for a in pn: 
+            if zmk == 0:
+                props = {
+                    'id': i[zmk]
+                }
+                
+                zmk += 1
+            props[a] = i[zmk]
+            zmk += 1
+        proportions.append(props)
+    return(proportions)
+   
+def add_prop(*proportions):
+    conn = sql.connect("training'sDictionary.db")
+    propNames = conn.execute("Select name from propCol")
+    conn.commit()
+        
+    query = "Insert Into 'proportions'("
+    add = ""
+    for i in propNames:
+        query = (f"{query}{add}'{i[0]}'")
+        add = ", "
+        
+    query = (f"{query}) values(")
+    add = ""
+    for i in proportions:
+        try:
+            index = i.index("-")
+            query = (f"{query}{add}'{i}'")
+        except:
+            query = (f"{query}{add}{i}")
+            
+        add = ", "
+    query = query + ")"
+    conn.execute(query)
+    conn.commit()
+    conn.close()
+
+def update_prop(id, *proportions):
+    conn = sql.connect("training'sDictionary.db")
+    propNames = conn.execute("Select name from propCol")
+    conn.commit()
+        
+    
+    
+    propN = []
+    for i in propNames:
+        propN.append(i[0])
+    
+    query = "UPDATE `proportions` SET  "
+    add = ""
+    a = 0
+    for i in proportions:
+        try:
+            index = i.index("-")
+            query = (f"{query}{add}`{propN[a]}` = '{i}'")
+        except:
+            query = (f"{query}{add}`{propN[a]}` = {i}")
+            
+        add = ", "
+        a += 1
+    query = (f"{query} WHERE `id` = {id}")
+    print(query)
+    conn.execute(query)
+    conn.commit()
+    conn.close()
+ 
 @app.route("/home")
 @app.route("/")
 def main():
@@ -20,7 +105,11 @@ def settings():
 
 @app.route("/calculator", methods = ["POST","GET"])
 def calc():
-    return render_template("calculator.html")
+    datetime = dt.datetime.now().strftime("%Y-%m-%d")
+    prop = read_prop()
+    pn = read_prop_cols()
+    
+    return render_template("calculator.html", prop = prop, pn = pn, datetime = datetime)
 
 @app.route("/addProportions", methods = ["POST"])
 def addprop():
@@ -31,9 +120,9 @@ def addprop():
         for n in fnames:
             a = request.form[n]
             prop.append(a)
-            
-        for i in prop:
-            print(i)
+        
+        
+        add_prop(*prop)
         return redirect("/calculator")
     else:
         return redirect("/calculator")
@@ -42,5 +131,23 @@ def addprop():
 def diction():
     return render_template("dictionary.html")
 
+@app.route("/updateProportions", methods = ["GET"])
+def updateprop():
+    if request.method == "GET":
+        udata = request.args
+        prop = []
+        
+        for n in udata:
+            a = request.args[n]
+            if n != "id":
+                prop = a.split(",")
+            else:
+                id = a
+        
+        update_prop(id, *prop)
+        return redirect("/calculator")
+    else:
+        return redirect("/calculator")
+   
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=420,debug=True)
